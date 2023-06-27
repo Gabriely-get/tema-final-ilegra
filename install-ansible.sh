437ef2c5bb06a5ab8b57a05d830eda783fe68075
Gabriely Willian <65106495+Gabriely-get@users.noreply.github.com>
2023-06-27 16:43:18 -0300

    Update install-ansible.sh

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo amazon-linux-extras install ansible2 -y
+sudo yum install -y amazon-linux-extras
+sudo amazon-linux-extras enable ansible2
+sudo yum install -y ansible
 sudo amazon-linux-extras install epel -y
