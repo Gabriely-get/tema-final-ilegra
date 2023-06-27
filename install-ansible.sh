7bfa85c443c4a47117addd38f061a3a779b9dc07
ilegra <gabrielysantos.informatica@gmail.com>
2023-06-27 18:58:09 -0300

    Delete files

--- install-ansible.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo yum update -y
-sudo yum install -y amazon-linux-extras
-sudo amazon-linux-extras enable ansible2
-sudo yum install -y ansible
-sudo amazon-linux-extras install epel -y
