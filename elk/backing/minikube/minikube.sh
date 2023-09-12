8598e98c6c02b5e80fb4aefe732aa88cbac2123d
Gabriely Willian <65106495+Gabriely-get@users.noreply.github.com>
2023-09-11 22:14:38 -0300

    Update minikube.sh

--- elk/backing/minikube/minikube.sh
+++ elk/backing/minikube/minikube.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 minikube delete
-minikube start --static-ip 192.168.49.2
+minikube start --static-ip 192.168.49.2 --memory 3000
 minikube addons enable ingress
 
 sleep 60s
 
 minikube kubectl -- apply -f /home/ec2-user/minikube/elasticsearch.yaml
 minikube kubectl -- apply -f /home/ec2-user/minikube/elasticsearch-ingress.yaml
+
+minikube kubectl -- apply -f /home/ec2-user/minikube/kibana.yaml
+minikube kubectl -- apply -f /home/ec2-user/minikube/kibana-ingress.yaml
 
 sudo systemctl enable nginx
 sudo systemctl restart nginx
